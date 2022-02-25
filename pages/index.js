41d7d6c8c354b96e4ea70b335e1d40e3d4f85911
furkan-cloud <firstinfirstout2020@gmail.com>
2022-02-25 16:53:10 +0300

    pages, event list, event item and dummy data added

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { getFeaturedEvents } from "../dummy-data";
+import EventList from "../components/events/event-list";
+
+const HomePage = () => {
+  const featuredEvents = getFeaturedEvents();
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
+};
+
+export default HomePage;
