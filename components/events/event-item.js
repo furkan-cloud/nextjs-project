e89ff922b1454c8bb1be64240cfacb83fc3a38b4
furkan-cloud <firstinfirstout2020@gmail.com>
2022-02-27 01:37:06 +0300

    styles added

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import classes from "./evet-item.module.css";
 
 const EventItem = (props) => {
   const { title, image, date, location, id } = props;
@@ -14,19 +15,19 @@ const EventItem = (props) => {
   const exploreLink = `/events/${id}`;
 
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + image} alt={title} />
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{title}</h2>
-          <div>
+          <div className={classes.date}>
             <time>{humanReadableDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
             <address>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}>Explore Event</Link>
         </div>
       </div>
--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,10 +1,11 @@
 import React from "react";
 import EventItem from "./event-item";
+import classes from "./evet-list.module.css"
 
 const EventList = (props) => {
   const { items } = props;
   return (
-    <ul>
+    <ul className={classes.list}> 
       {items.map((event) => (
         <EventItem
           key={event.id}
