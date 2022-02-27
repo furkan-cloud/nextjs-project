5092e3aed86060bdb0f156466ccc99cccba22d31
furkan-cloud <firstinfirstout2020@gmail.com>
2022-02-27 03:08:24 +0300

    error and result title component added, slug page added

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,6 +1,10 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { getEventById } from "../../dummy-data";
+import ErrorAlert from "../../components/error-alert/error-alert";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 const EventDetailPage = () => {
   const router = useRouter();
@@ -9,7 +13,11 @@ const EventDetailPage = () => {
   const event = getEventById(eventId);
 
   if (!event) {
-    return <p>No event found</p>;
+    return (
+      <ErrorAlert>
+        <p>No event found</p>
+      </ErrorAlert>
+    );
   }
 
   return (
